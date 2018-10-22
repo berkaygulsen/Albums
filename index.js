054fc1df83515647e8141ae39d964d9e7fa96211
berkaygulsen <berkaygulsen@gmail.com>
2018-10-23 02:43:20 +0300

    Final

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as HelloWorld} from './app.json';
+
+AppRegistry.registerComponent(HelloWorld, () => App);
